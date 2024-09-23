1cc3b227b11c028adff2c2cd03c06f3ac49601c6
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-09-23 17:34:51 +0900

    add: metadataを追加

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,11 @@ import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/edit-form";
 import { notFound } from "next/navigation";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices Edit',
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id
